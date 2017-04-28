e5079a85327a06cdd22f354b8270496672d95438
sundeepkotari78 <sundeep.kotari78@gmail.com>
2017-04-28 17:01:57 -0500

    Angular 2 - Vehicle Sales

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,162 @@
+
+import {Component, OnInit} from '@angular/core';
+import {nvD3} from 'ng2-nvd3';
+import { FormsModule } from '@angular/forms';
+declare let d3: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  public gridData;
+  public piechartOptions;
+  public piechartData;
+  public multibarChartData;
+  public multibarChartOptions;
+  ngOnInit(){
+
+  this.piechartOptions = {
+            chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 5,
+                        right: 35,
+                        bottom: 5,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        this.piechartData = [
+            {
+                key: "Texas",
+                y: 5000
+            },
+            {
+                key: "Florida",
+                y: 2000
+            },
+            {
+                key: "New Jersey",
+                y: 9000
+            },
+            {
+                key: "Arkansas",
+                y: 7000
+            },
+            {
+                key: "Washington",
+                y: 4000
+            },
+            {
+                key: "California",
+                y: 3000
+            },
+            {
+                key: "Missouri",
+                y: 2500
+            }
+        ];
+  this.multibarChartOptions = {
+            chart: {
+                type: 'multiBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 45,
+                    left: 45
+                },
+                clipEdge: true,
+                duration: 500,
+                stacked: true,
+                xAxis: {
+                    axisLabel: 'Quarters',
+                    showMaxMin: false,
+                    tickFormat: function(d){
+                        return d3.format(',f')(d);
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Y Axis',
+                    axisLabelDistance: -20,
+                    tickFormat: function(d){
+                        return d3.format(',.1f')(d);
+                    }
+                }
+            }
+        };
+
+        this.multibarChartData = generateData();
+
+        /* Random Data Generator (took from nvd3.org) */
+        function generateData() {
+            return stream_layers(3,10+Math.random()*10,.1).map(function(data, i) {
+                return {
+                    key: 'Stream' + i,
+                    values: data
+                };
+            });
+        }
+
+        /* Inspired by Lee Byron's test data generator. */
+        function stream_layers(n, m, o) {
+            if (arguments.length < 3) o = 0;
+            function bump(a) {
+                var x = 1 / (.1 + Math.random()),
+                    y = 2 * Math.random() - .5,
+                    z = 10 / (.1 + Math.random());
+                for (var i = 0; i < m; i++) {
+                    var w = (i / m - y) * z;
+                    a[i] += x * Math.exp(-w * w);
+                }
+            }
+            return d3.range(n).map(function() {
+                var a = [], i;
+                for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+                for (i = 0; i < 5; i++) bump(a);
+                return a.map(stream_index);
+            });
+        }
+
+        /* Another layer generator using gamma distributions. */
+        function stream_waves(n, m) {
+            return d3.range(n).map(function(i) {
+                return d3.range(m).map(function(j) {
+                    var x = 20 * j / m - i / 3;
+                    return 2 * x * Math.exp(-.5 * x);
+                }).map(stream_index);
+            });
+        }
+
+        function stream_index(d, i) {
+            return {x: i, y: Math.max(0, d)};
+        }
+
+
+    this.gridData = [{name: "Moroni", age: 50},
+                     {name: "Tiancum", age: 43},
+                     {name: "Jacob", age: 27},
+                     {name: "Nephi", age: 29},
+                     {name: "Enos", age: 34}];
+  }
+
+  resetData(){
+  //Erase data
+        this.piechartData = [];
+        this.multibarChartData = []
+  }
+
+}
